37155e90eeaa85a12ef22dbf8ca5c4f89ca09cd0
Ashish <ashish.jat@gmail.com>
2011-05-15 15:47:50 +0530

    Done with statis pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
   def home
+	@title = "Home"
   end
 
   def contact
+	@title = "Contact Us"
   end
 
+  def about
+  	@title = "About Us"
+  end
+  
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,12 +1,18 @@
 require 'spec_helper'
 
 describe PagesController do
-
+	render_views
+	
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
+	
+	it "should have the right title" do
+		get 'home'
+		response.should have_selector("title", :content => "Home")
+	end
   end
 
   describe "GET 'contact'" do
@@ -14,6 +20,12 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+
+	it "should have the right title" do
+		get 'contact'
+		response.should have_selector("title", :content => "Contact")
+	end
+
   end
 
 end
